883d3f58115508a2792fe6587ecba7c8dc2da045
kimphuongnt <kimphuongnt.dev@gmail.com>
2024-09-27 10:16:09 +0700

    add dynamic control: menu header

---
+++ DynamicControl/menucontrol.Designer.cs
@@ -0,0 +1,179 @@
+﻿namespace DynamicControl
+{
+    partial class menucontrol
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnThem = new Sunny.UI.UISymbolButton();
+            this.btnXoa = new Sunny.UI.UISymbolButton();
+            this.btnSua = new Sunny.UI.UISymbolButton();
+            this.btnTim = new Sunny.UI.UISymbolButton();
+            this.btnHuy = new Sunny.UI.UISymbolButton();
+            this.btnLuu = new Sunny.UI.UISymbolButton();
+            this.btnThoat = new Sunny.UI.UISymbolButton();
+            this.SuspendLayout();
+            // 
+            // btnThem
+            // 
+            this.btnThem.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnThem.FillColor = System.Drawing.Color.CornflowerBlue;
+            this.btnThem.FillHoverColor = System.Drawing.Color.CornflowerBlue;
+            this.btnThem.Font = new System.Drawing.Font("Arial", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnThem.Location = new System.Drawing.Point(6, 3);
+            this.btnThem.MinimumSize = new System.Drawing.Size(1, 1);
+            this.btnThem.Name = "btnThem";
+            this.btnThem.Size = new System.Drawing.Size(142, 45);
+            this.btnThem.Symbol = 61543;
+            this.btnThem.SymbolSize = 30;
+            this.btnThem.TabIndex = 0;
+            this.btnThem.Text = "Thêm";
+            this.btnThem.TipsFont = new System.Drawing.Font("Microsoft Sans Serif", 9F);
+            // 
+            // btnXoa
+            // 
+            this.btnXoa.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnXoa.FillColor = System.Drawing.Color.CornflowerBlue;
+            this.btnXoa.FillHoverColor = System.Drawing.Color.CornflowerBlue;
+            this.btnXoa.Font = new System.Drawing.Font("Arial", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnXoa.Location = new System.Drawing.Point(161, 3);
+            this.btnXoa.MinimumSize = new System.Drawing.Size(1, 1);
+            this.btnXoa.Name = "btnXoa";
+            this.btnXoa.Size = new System.Drawing.Size(142, 45);
+            this.btnXoa.Symbol = 61526;
+            this.btnXoa.SymbolSize = 30;
+            this.btnXoa.TabIndex = 1;
+            this.btnXoa.Text = "Xóa";
+            this.btnXoa.TipsFont = new System.Drawing.Font("Microsoft Sans Serif", 9F);
+            // 
+            // btnSua
+            // 
+            this.btnSua.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnSua.FillColor = System.Drawing.Color.CornflowerBlue;
+            this.btnSua.FillHoverColor = System.Drawing.Color.CornflowerBlue;
+            this.btnSua.Font = new System.Drawing.Font("Arial", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSua.Location = new System.Drawing.Point(316, 3);
+            this.btnSua.MinimumSize = new System.Drawing.Size(1, 1);
+            this.btnSua.Name = "btnSua";
+            this.btnSua.Size = new System.Drawing.Size(142, 45);
+            this.btnSua.Symbol = 61508;
+            this.btnSua.SymbolSize = 30;
+            this.btnSua.TabIndex = 2;
+            this.btnSua.Text = "Sửa";
+            this.btnSua.TipsFont = new System.Drawing.Font("Microsoft Sans Serif", 9F);
+            // 
+            // btnTim
+            // 
+            this.btnTim.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnTim.FillColor = System.Drawing.Color.CornflowerBlue;
+            this.btnTim.FillHoverColor = System.Drawing.Color.CornflowerBlue;
+            this.btnTim.Font = new System.Drawing.Font("Arial", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnTim.Location = new System.Drawing.Point(626, 3);
+            this.btnTim.MinimumSize = new System.Drawing.Size(1, 1);
+            this.btnTim.Name = "btnTim";
+            this.btnTim.Size = new System.Drawing.Size(142, 45);
+            this.btnTim.Symbol = 61442;
+            this.btnTim.SymbolSize = 30;
+            this.btnTim.TabIndex = 3;
+            this.btnTim.Text = "Tìm";
+            this.btnTim.TipsFont = new System.Drawing.Font("Microsoft Sans Serif", 9F);
+            // 
+            // btnHuy
+            // 
+            this.btnHuy.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnHuy.FillColor = System.Drawing.Color.CornflowerBlue;
+            this.btnHuy.FillHoverColor = System.Drawing.Color.CornflowerBlue;
+            this.btnHuy.Font = new System.Drawing.Font("Arial", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnHuy.Location = new System.Drawing.Point(781, 3);
+            this.btnHuy.MinimumSize = new System.Drawing.Size(1, 1);
+            this.btnHuy.Name = "btnHuy";
+            this.btnHuy.Size = new System.Drawing.Size(142, 45);
+            this.btnHuy.Symbol = 557577;
+            this.btnHuy.SymbolSize = 30;
+            this.btnHuy.TabIndex = 4;
+            this.btnHuy.Text = "Hủy";
+            this.btnHuy.TipsFont = new System.Drawing.Font("Microsoft Sans Serif", 9F);
+            // 
+            // btnLuu
+            // 
+            this.btnLuu.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnLuu.FillColor = System.Drawing.Color.CornflowerBlue;
+            this.btnLuu.FillHoverColor = System.Drawing.Color.CornflowerBlue;
+            this.btnLuu.Font = new System.Drawing.Font("Arial", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnLuu.Location = new System.Drawing.Point(471, 3);
+            this.btnLuu.MinimumSize = new System.Drawing.Size(1, 1);
+            this.btnLuu.Name = "btnLuu";
+            this.btnLuu.Size = new System.Drawing.Size(142, 45);
+            this.btnLuu.Symbol = 61639;
+            this.btnLuu.SymbolSize = 30;
+            this.btnLuu.TabIndex = 5;
+            this.btnLuu.Text = "Lưu";
+            this.btnLuu.TipsFont = new System.Drawing.Font("Microsoft Sans Serif", 9F);
+            // 
+            // btnThoat
+            // 
+            this.btnThoat.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnThoat.FillColor = System.Drawing.Color.CornflowerBlue;
+            this.btnThoat.FillHoverColor = System.Drawing.Color.CornflowerBlue;
+            this.btnThoat.Font = new System.Drawing.Font("Arial", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnThoat.Location = new System.Drawing.Point(938, 3);
+            this.btnThoat.MinimumSize = new System.Drawing.Size(1, 1);
+            this.btnThoat.Name = "btnThoat";
+            this.btnThoat.Size = new System.Drawing.Size(142, 45);
+            this.btnThoat.Symbol = 559513;
+            this.btnThoat.SymbolSize = 30;
+            this.btnThoat.TabIndex = 6;
+            this.btnThoat.Text = "Thoát";
+            this.btnThoat.TipsFont = new System.Drawing.Font("Microsoft Sans Serif", 9F);
+            // 
+            // menucontrol
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.btnThoat);
+            this.Controls.Add(this.btnLuu);
+            this.Controls.Add(this.btnHuy);
+            this.Controls.Add(this.btnTim);
+            this.Controls.Add(this.btnSua);
+            this.Controls.Add(this.btnXoa);
+            this.Controls.Add(this.btnThem);
+            this.Name = "menucontrol";
+            this.Size = new System.Drawing.Size(1092, 53);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Sunny.UI.UISymbolButton btnThem;
+        private Sunny.UI.UISymbolButton btnXoa;
+        private Sunny.UI.UISymbolButton btnSua;
+        private Sunny.UI.UISymbolButton btnTim;
+        private Sunny.UI.UISymbolButton btnHuy;
+        private Sunny.UI.UISymbolButton btnLuu;
+        private Sunny.UI.UISymbolButton btnThoat;
+    }
+}
---
+++ DynamicControl/menucontrol.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics.Eventing.Reader;
+
+namespace DynamicControl
+{
+    public partial class menucontrol : UserControl
+    {
+        public enum Status
+        {
+            View, Edit
+        }
+        public enum ButtonType
+        {
+            Add, Delete, Edit, Save, Exit, Cancel, Search
+        }
+        private Status currentStatus;
+
+        public menucontrol()
+        {
+            InitializeComponent();
+        }
+        public delegate void ButtonClickedHandler(object sender, ButtonType buttonType, EventArgs e);
+        public event ButtonClickedHandler ButtonClicked;
+
+        protected virtual void OnButtonClicked(object sender, ButtonType buttonType, EventArgs e)
+        {
+            ButtonClicked?.Invoke(sender, buttonType, e);
+        }
+        private void SetStatus(Status status)
+        {
+            currentStatus = status;
+            switch(status)
+            {
+                case Status.View:
+                    btnThem.Enabled = true;
+                    btnSua.Enabled = true;
+                    btnXoa.Enabled = true;
+                    btnThoat.Enabled = true;
+                    btnLuu.Enabled = false;
+                    btnHuy.Enabled = false;
+                    break;
+
+                case Status.Edit:
+                    btnThem.Enabled = false;
+                    btnSua.Enabled = false;
+                    btnXoa.Enabled = false;
+                    btnThoat.Enabled = false;
+                    btnLuu.Enabled = true;
+                    btnHuy.Enabled = true;
+                    break;
+            }
+        }
+        private void btnThem_Click(object sender, EventArgs e)
+        {
+            SetStatus(Status.Edit);
+            OnButtonClicked(sender, ButtonType.Add, e);
+        }
+
+        private void btnSua_Click(object sender, EventArgs e)
+        {
+            SetStatus(Status.Edit);
+            OnButtonClicked(sender, ButtonType.Edit, e);
+        }
+
+        private void btnXoa_Click(object sender, EventArgs e)
+        {
+            SetStatus(Status.View);
+            OnButtonClicked(sender, ButtonType.Delete, e);
+        }
+
+        private void btnLuu_Click(object sender, EventArgs e)
+        {
+            SetStatus(Status.View);
+            OnButtonClicked(sender, ButtonType.Save, e);
+        }
+
+        private void btnHuy_Click(object sender, EventArgs e)
+        {
+            SetStatus(Status.View);
+            OnButtonClicked(sender, ButtonType.Cancel, e);
+        }
+
+        private void btnThoat_Click(object sender, EventArgs e)
+        {
+            OnButtonClicked(sender, ButtonType.Exit, e);
+        }
+    }
+}
